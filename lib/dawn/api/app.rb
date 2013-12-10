6aa27e93d7202b28e3d2a0967daedebaac02cc06
IceDragon200 <mistdragon100@gmail.com>
2013-12-10 14:47:32 -0500

    api/app update
    Fixed all requests, renamed options to data

--- lib/dawn/api/app.rb
+++ lib/dawn/api/app.rb
@@ -1,50 +1,69 @@
 module Dawn
   class App
 
-    attr_reader :options
+    attr_reader :data
 
-    def initialize(options)
-      @options = options
+    def initialize(hsh)
+      @data = hsh["app"]
+    end
+
+    def name
+      data["name"]
     end
 
     def id
-      options["id"]
+      data["id"]["$oid"]
     end
 
-    def update
+    def update(options={})
       Dawn.request(
         expects: 200,
-        method: :post,
+        method: :patch,
         path: "/apps/#{id}",
         query: options
       )
     end
 
-    def destroy
+    def destroy(options={})
       Dawn.request(
-        expects: 203,
+        expects: 204,
         method: :delete,
         path: "/apps/#{id}",
         query: options
       )
     end
 
+    def self.all(options={})
+      resp = Dawn.request(
+        expects: 200,
+        method: :get,
+        path: "/apps",
+        query: options
+      )
+      JSON.load(resp.body).map { |hsh| new(hsh) }
+    end
+
     def self.create(options)
-      new Dawn.request(
+      new JSON.load(Dawn.request(
         expects: 200,
         method: :post,
         path: '/apps',
         query: options
-      )
+      ).body)
     end
 
     def self.find(options)
-      new Dawn.request(
+      new JSON.load(Dawn.request(
         expects: 200,
-        method: :post,
+        method: :get,
         path: "/apps/#{options[:id]}",
         query: options
-      )
+      ).body)
+    end
+
+    def self.destroy(options)
+      app = find(options)
+      app.destroy if app
     end
 
   end
