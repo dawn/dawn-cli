c69aa9abad7f01395107d3ff047457c77f4add51
IceDragon200 <mistdragon100@gmail.com>
2013-12-16 14:20:08 -0500

    Cosmetic space changes

--- lib/dawn/cli.rb
+++ lib/dawn/cli.rb
@@ -1,4 +1,4 @@
-require 'dawn/api'
-require 'dawn/cli/helpers'
-require 'dawn/cli/commands'
-require 'dawn/cli/version' # CLI::Version information
+require 'dawn/api'          # Dawn::Api
+require 'dawn/cli/version'  # CLI::Version information
+require 'dawn/cli/helpers'  # CLI Helpers Module
+require 'dawn/cli/commands' # CLI Commands
