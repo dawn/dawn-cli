ad0de0e16e56a0344b5b127b135f75126761ae38
IceDragon200 <mistdragon100@gmail.com>
2013-12-16 11:09:34 -0500

    Updated login message and double quotes

--- lib/dawn/cli/login.rb
+++ lib/dawn/cli/login.rb
@@ -1,8 +1,8 @@
-command 'login' do |c|
+command "login" do |c|
   c.action do |args, options|
     usn = ask "Username: "
     psw = password "Password: "
     Dawn.authenticate(username: usn, password: psw)
-    puts "your login details have been saved to your .netrc"
+    puts " ! login details have been saved to your .netrc"
   end
 end
