85935abec3895e491bcbcc5304bb54c2c9fb480f
IceDragon200 <mistdragon100@gmail.com>
2013-12-16 14:13:17 -0500

    Dawn::CLI::Helpers module

---
+++ lib/dawn/cli/helpers.rb
@@ -0,0 +1,17 @@
+module Dawn
+  module Helpers
+
+    ## swiped from Heroku
+    def has_git?
+      %x{ git --version }
+      $?.success?
+    end
+
+    def git(args)
+      return "" unless has_git?
+      flattened_args = [args].flatten.compact.join(" ")
+      %x{ git #{flattened_args} 2>&1 }.strip
+    end
+
+  end
+end
