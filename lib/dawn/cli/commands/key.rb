476a28047d6b245d1247a3f42a947d2547776a56
IceDragon200 <mistdragon100@gmail.com>
2013-12-16 19:36:37 -0500

    Added description for key commands

--- lib/dawn/cli/commands/key.rb
+++ lib/dawn/cli/commands/key.rb
@@ -1,10 +1,12 @@
 command "key:add" do |c|
+  c.description = "adds your local ssh_key"
   c.action do |args, options|
     Dawn::Key.add
   end
 end
 
 command "key:list" do |c|
+  c.description = "lists all your ssh_keys"
   c.action do |args, options|
     keys = Dawn::Key.all
     keys.each do |key|
@@ -14,6 +16,7 @@ command "key:list" do |c|
 end
 
 command "key:get" do |c|
+  c.description = "retrieve a key by ID"
   c.action do |args, options|
     key_id = args.first
     puts Dawn::Key.find(id: key_id)
@@ -21,6 +24,7 @@ command "key:get" do |c|
 end
 
 command "key:delete" do |c|
+  c.description = "delete a key by ID"
   c.action do |args, options|
     key_id = args.first
     Dawn::Key.destroy(id: key_id)
