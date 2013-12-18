5c42c1328ed1db17672d917bb950d5e021d35ccc
IceDragon200 <mistdragon100@gmail.com>
2013-12-18 18:07:38 -0500

    Env is now a Hash
    update > save

--- lib/dawn/api/app/env.rb
+++ lib/dawn/api/app/env.rb
@@ -1,25 +1,17 @@
 module Dawn
   class App
-    class Env
+    class Env < Hash
 
       attr_reader :app
-      attr_reader :data
 
       def initialize(app, data)
         @app = app
-        @data = data
-      end
-
-      def [](key)
-        data[key]
-      end
-
-      def []=(key, value)
-        data[key] = value
+        super
+        merge!(data)
       end
 
       def refresh(options={})
-        @data.merge(JSON.load(Dawn.request(
+        merge!(JSON.load(Dawn.request(
           expects: 200,
           method: :get,
           path: "/apps/#{app.id}/env",
@@ -27,12 +19,12 @@ module Dawn
         ).body)["env"])
       end
 
-      def update(options={})
+      def save(options={})
         Dawn.request(
           expects: 200,
           method: :put,
           path: "/apps/#{app.id}/env",
-          query: { env: data.merge(options) }
+          query: { env: merge(options) }
         )
       end
 
