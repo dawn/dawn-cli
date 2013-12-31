017d39868f002317858609c462a5ffbe1d65f9e7
IceDragon200 <mistdragon100@gmail.com>
2013-12-31 10:48:17 -0500

    Fixed formatter tables

--- lib/dawn/cli/formatter.rb
+++ lib/dawn/cli/formatter.rb
@@ -1,34 +1,44 @@
+def table_style
+  { width: 80 }
+end
+
 def print_apps(apps)
-  say Terminal::Table.new headings: ['ID', 'Name', 'Formation'] do |t|
-    apps.each do |app|
-      form = app.formation.map { |k,v| "#{k}: #{v}" }.join("\n")
-      t << [app.id, app.name, form]
-      t << :separator
-    end
+  table = Terminal::Table.new title: 'Apps',
+                              headings: ['ID', 'Name', 'Formation'],
+                              style: table_style
+  apps.each do |app|
+    form = app.formation.map { |k,v| "#{k}: #{v}" }.join("\n")
+    table << [app.id, app.name, form]
+    table << :separator
   end
+  say table
 end
 
 def print_drains(drains)
-  say Terminal::Table.new headings: ['ID', 'Name', 'URL'] do |t|
-    drains.each do |drain|
-      t << [drain.id, drain.name, drain.url]
-      t << :separator
-    end
+  table = Terminal::Table.new title: 'Drains',
+                              headings: ['ID', 'Name', 'URL'],
+                              style: table_style
+  drains.each do |drain|
+    table << [drain.id, drain.name, drain.url]
+    table << :separator
   end
+  say table
 end
 
 def print_gears(gears)
-  say Terminal::Table.new headings: ['ID', 'Name', 'Uptime'] do |t|
-    gears.each do |gear|
-      n = gear.uptime.to_i
-      if n > 0
-        scale  = TimeLord::Scale.new(n)
-        uptime = "#{scale.to_value} #{scale.to_unit}"
-      else
-        uptime = "just now"
-      end
-      t << [gear.id, gear.name, uptime]
-      t << :separator
+  table = Terminal::Table.new title: 'Gears',
+                              headings: ['ID', 'Name', 'Uptime'],
+                              style: table_style
+  gears.each do |gear|
+    n = gear.uptime.to_i
+    if n > 0
+      scale  = TimeLord::Scale.new(n)
+      uptime = "#{scale.to_value} #{scale.to_unit}"
+    else
+      uptime = "just now"
     end
+    table << [gear.id, gear.name, uptime]
+    table << :separator
   end
+  say table
 end
