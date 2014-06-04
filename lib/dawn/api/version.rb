b7cc8480a3caffb5cb9c719c65ff5c24d727fe4f
IceDragon200 <mistdragon100@gmail.com>
2014-06-04 14:49:03 -0500

    API Version bump 0.7.0

--- lib/dawn/api/version.rb
+++ lib/dawn/api/version.rb
@@ -2,7 +2,7 @@ module Dawn
   module API
     module Version
       MAJOR = 0
-      MINOR = 6
+      MINOR = 7
       PATCH = 0
       BUILD = nil
       STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".").freeze
