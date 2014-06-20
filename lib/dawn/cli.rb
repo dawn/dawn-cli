be718027d1216cbfbd7bebe50994c978f329dcba
IceDragon200 <mistdragon100@gmail.com>
2014-06-20 01:08:06 -0500

    Updated lib/dawn/cli to load commands and docs

--- lib/dawn/cli.rb
+++ lib/dawn/cli.rb
@@ -1,4 +1,5 @@
 require 'dawn/api'             # Dawn::Api
 require 'dawn/cli/version'     # CLI::Version information
 require 'dawn/cli/patches'     # Various Monkey Patches
-require 'dawn/cli/application' # Various Monkey Patches
+require 'dawn/cli/commands'    # CLI Commands
+require 'dawn/cli/docs'
