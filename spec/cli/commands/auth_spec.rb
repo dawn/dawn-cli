7237fb807f4476b47abba7a3e9c6c81229e4e15f
IceDragon200 <mistdragon100@gmail.com>
2014-06-24 14:14:42 -0500

    Rearranged auth spec

--- spec/cli/commands/auth_spec.rb
+++ spec/cli/commands/auth_spec.rb
@@ -1,15 +1,13 @@
 require 'cli_spec_helper'
 
 describe Dawn::CLI::Auth, :vcr do
-
   context ".login" do
-    it "should login using proper credentials" do
-      Dawn::CLI::Auth.login("IceDragon", "creampuff")
-    end
-
     it "should not login using invalid credentials" do
       expect { Dawn::CLI::Auth.login("SomeGuy", "withsomepassword") }.to raise_error(SystemExit)
     end
-  end
 
+    it "should login using proper credentials" do
+      Dawn::CLI::Auth.login("IceDragon", "creampuff")
+    end
+  end
 end
