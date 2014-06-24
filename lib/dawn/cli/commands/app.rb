717308ed73ac0c59e17bdd00f9cb4fb7352134ec
IceDragon200 <mistdragon100@gmail.com>
2014-06-23 19:21:35 -0500

    Move .create from Local to App

--- lib/dawn/cli/commands/app.rb
+++ lib/dawn/cli/commands/app.rb
@@ -6,6 +6,16 @@ module Dawn
 
       extend Dawn::CLI::Helpers
 
+      # "Create a new dawn App (with git; setup)"
+      def self.create(appname=nil)
+        app = try_create_app appname
+        # since its possible for dawn to create a new app, with a random name
+        # setting the appname again based on the real app's name is required
+        appname = app.name
+        git_add_dawn_remote app
+        say "\tAPP\t#{app.name}"
+      end
+
       # "Displays a list of all the apps you have deployed to dawn"
       def self.list
         say format_apps(Dawn::App.all)
--- lib/dawn/cli/commands/local.rb
+++ lib/dawn/cli/commands/local.rb
@@ -10,16 +10,6 @@ module Dawn
         Dawn::API.health_check
       end
 
-      # "Create a new dawn App (with git; setup)"
-      def self.create(appname=nil)
-        app = try_create_app appname
-        # since its possible for dawn to create a new app, with a random name
-        # setting the appname again based on the real app's name is required
-        appname = app.name
-        git_add_dawn_remote app
-        say "\tAPP\t#{app.name}"
-      end
-
     end
   end
 end
